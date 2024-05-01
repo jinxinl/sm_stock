1e9f55c62f212633d950ccbaf15aa42a9ad73a11
jinxinl <18906934953@qq.com>
2024-05-01 21:24:21 +0800

    平均收益率（未调试）

--- sm_MaxProfit.m
+++ sm_MaxProfit.m
@@ -16,5 +16,30 @@ AEG=data(srcline:dstline,10);
 AIR=data(srcline:dstline,11);
 
 %计算每日收益率
+A_profitRate=ProfitRate(A);
+AA_profitRate=ProfitRate(AA);
+ABC_profitRate=ProfitRate(ABC);
+ABCB_profitRate=ProfitRate(ABCB);
+ACLS_profitRate=ProfitRate(ACLS);
+ACNB_profitRate=ProfitRate(ACNB);
+ADBE_profitRate=ProfitRate(ADBE);
+ADP_profitRate=ProfitRate(ADP);
+AEG_profitRate=ProfitRate(AEG);
+AIR_profitRate=ProfitRate(AIR);
+
+mean_Arate=mean(A_profitRate);
+mean_AArate=mean(AA_profitRate);
+mean_ABCrate=mean(ABC_profitRate);
+mean_ABCBrate=mean(ABCB_profitRate);
+mean_ACLSrate=mean(ACLS_profitRate);
+mean_ACNBrate=mean(ACNB_profitRate);
+mean_ADBErate=mean(ADBE_profitRate);
+mean_ADPrate=mean(ADP_profitRate);
+mean_AEGrate=mean(AEG_profitRate);
+mean_AIRrate=mean(AIR_profitRate);
+
+disp(A_profitRate);
+
+
 
 
