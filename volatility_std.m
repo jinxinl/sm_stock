5f339f719b2da4c12b76d6760fbec2c8f66aae87
yanyi1128 <2411527467@qq.com>
2024-05-03 17:21:46 +0800

    将两种求风险率的方式封装成了函数

--- volatility_std.m
+++ volatility_std.m
@@ -1,15 +1,7 @@
+function volatilityRate=volatility_std()
 run('sm_MaxProfit.m');%运行文件，得到平均收益率
 %计算每支股票的标准差，用标准差来估计平均风险率
-volatility_ate
-volatility_Arate=std(A_profitRate);
-volatility_AArate=std(AA_profitRate);
-volatility_ABCrate=std(ABC_profitRate);
-volatility_ABCBrate=std(ABCB_profitRate);
-volatility_ACLSrate=std(ACLS_profitRate);
-volatility_ACNBrate=std(ACNB_profitRate);
-volatility_ADBErate=std(ADBE_profitRate);
-volatility_ADPrate=std(ADP_profitRate);
-volatility_AEGrate=std(AEG_profitRate);
-volatility_AIRrate=std(AIR_profitRate);
-
-disp(volatility_Arate);
+volatilityRate=zeros(1,10);
+for i=1:10
+    volatilityRate(i)=mean(profitRate(1:end,i));
+end
